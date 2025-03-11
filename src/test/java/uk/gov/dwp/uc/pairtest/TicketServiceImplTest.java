7b58713ee71390af8497c840dc30b4b61b23243e
Abubakar Amasa <amasaabubakar@gmail.com>
2025-03-11 12:52:12 +0000

    refactor-test-cases

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -55,18 +55,25 @@ public class TicketServiceImplTest {
     @Test
     public void shouldAllowPurchaseOfTicket(){
         var accountId = 1L;
-        //TOTAL NUMBER OF TICKET =25
-        //ADULT TICKET COST = 250
-        //CHILD TICKET COSt = 210
-        //INFANT TICKET = 0
-        //INFANT GETS NO SEAT
-        TicketTypeRequest request1 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 10);
-        TicketTypeRequest request2 = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 14);
-        TicketTypeRequest request3 = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1);
+        //SEATS TO RESERVE
+        final var NUM_OF_ADULT = 10;
+        final var NUM_OF_CHILDREN = 14;
+        final var NUM_OF_INFANT = 1;
+
+        //EXPECTED COST OF TICKET
+        final var ADULT_TICKET_COST = NUM_OF_ADULT * 25;
+        final var CHILD_TICKET_COST = NUM_OF_CHILDREN * 15;
+        final var INFANT_TICKET_COST = NUM_OF_INFANT * 0;  // Infants have no cost but do not get a seat
+        final var EXPECTED_TOTAL_COST = ADULT_TICKET_COST +CHILD_TICKET_COST+INFANT_TICKET_COST;
+
+        TicketTypeRequest request1 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, NUM_OF_ADULT);
+        TicketTypeRequest request2 = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, NUM_OF_CHILDREN);
+        TicketTypeRequest request3 = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, NUM_OF_INFANT);
         TicketTypeRequest[] tickets = {request1, request2, request3};
 
         ticketService.purchaseTickets(accountId, tickets);
-        verify(ticketPaymentService, times(1)).makePayment(accountId, 460);
-        verify(seatReservationService, times(1)).reserveSeat(accountId, 24);
+        verify(ticketPaymentService, times(1)).makePayment(accountId, EXPECTED_TOTAL_COST);
+        final var EXPECTED_NUMBER_OF_SEAT_TO_RESERVE = NUM_OF_ADULT + NUM_OF_CHILDREN;
+        verify(seatReservationService, times(1)).reserveSeat(accountId, EXPECTED_NUMBER_OF_SEAT_TO_RESERVE);
     }
 }
